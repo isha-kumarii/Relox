ac8cf57af7c102c7e1db915626a8115d5d6ac4eb
isha <singhisha116@gmail.com>
2024-01-09 18:59:50 +0530

    Edited CSS

---
+++ nf_components/navbar.js
@@ -0,0 +1,12 @@
+function navbar(){
+    return `
+    <div id="left">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Relox_2015_logo.svg/2560px-Relox_2015_logo.svg.png" alt="" id="logo">
+    </div>
+    <div id="right">
+        <h3><a href="" id="right_menu">Sign In</a></h3>
+    </div>
+    `
+}
+
+export default navbar;
